9b6f216645fdc358b2da03082622eee7486b69ce
Charlie <charliewang92@gmail.com>
2016-05-02 16:13:06 -0400

    can monitor basic battery info

--- QuickShoppe/app/src/main/java/com/quickshoppe/charlie/quickshoppe/BatteryActivity.java
+++ QuickShoppe/app/src/main/java/com/quickshoppe/charlie/quickshoppe/BatteryActivity.java
@@ -7,15 +7,59 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.content.IntentFilter;
 
 public class BatteryActivity extends ActionBarActivity {
     Button returnButton;
+    private TextView batteryInfo;
+    private ImageView imageBatteryState;
+
+    private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int  health= intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
+            int  icon_small= intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
+            int  level= intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+            boolean  present= intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+            int  scale= intent.getIntExtra(BatteryManager.EXTRA_SCALE,0);
+            int  status= intent.getIntExtra(BatteryManager.EXTRA_STATUS,0);
+            String  technology= intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+            int  temperature= intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0);
+            int  voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
+
+
+            batteryInfo.setText(
+                    "Health: "+health+"\n"+
+                            "Icon Small:"+icon_small+"\n"+
+                            "Level: "+level+"\n"+
+                            "Plugged: "+plugged+"\n"+
+                            "Present: "+present+"\n"+
+                            "Scale: "+scale+"\n"+
+                            "Status: "+status+"\n"+
+                            "Technology: "+technology+"\n"+
+                            "Temperature: "+temperature+"\n"+
+                            "Voltage: "+voltage+"\n");
+            imageBatteryState.setImageResource(icon_small);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battery);
+
+        batteryInfo=(TextView)findViewById(R.id.textViewBatteryInfo);
+        imageBatteryState=(ImageView)findViewById(R.id.imageViewBatteryState);
+
+        this.registerReceiver(this.batteryInfoReceiver,	new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
 
         returnButton = (Button) findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
